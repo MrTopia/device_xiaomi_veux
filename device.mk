3b0ec3b8617e6d363dcff6428ac93afd37ebc107
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-10-16 11:13:38 +0000

    veux: Define AAPT configuration
    Change-Id: Ic12fc243a6b86a0972ef5dc0bb26148be540be57
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
