7a1bb086bc2d7aabc8b9e2705213d55ff96b151d
Zinadin Zidan <zidan.roking@gmail.com>
2023-07-01 06:52:14 +0300

    veux: Guard qti thermal 2.0 service hals
     * Fixes:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
    
    Change-Id: I12623503caa87ce99ce8ab76aadee77e15b167f7
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- device.mk
+++ device.mk
@@ -458,8 +458,10 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
+endif
 
 # Update engine
 PRODUCT_PACKAGES += \
