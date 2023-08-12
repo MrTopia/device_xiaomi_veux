e39f32f2a253f51ac0a378988a496ef347eb9835
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-08-12 13:51:31 +0300

    veux: Switch to common Xiaomi light HAL
    Change-Id: Ic773e6e411a5cf178bba3a61b20457d62d177db0
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.xiaomi
+
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
