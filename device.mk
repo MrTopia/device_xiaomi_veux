993983f42b0115335f6cebc245e7a7dc2b154395
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-14 09:08:32 +0100

    veux: Add support for Dolby Atmos
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I1219e0ac9965a0a6a1e128c2560ca74df3947ea0
    Signed-off-by: Coptan99 <omarcoptan9@gmail.com>
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- device.mk
+++ device.mk
@@ -247,7 +247,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# Net
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
+
+# Network
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
 
