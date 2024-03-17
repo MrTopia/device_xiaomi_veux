b9d193f8ba9806d1b95622db61711c2758c3ac9d
MrTopia <redkarmakar@gmail.com>
2024-03-17 23:49:57 +0530

    veux: AOSP Bring Up
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,30 @@
+echo "Cloning required stuffs..."
+
+echo "Cloning Kernel..."
+# Kernel
+git clone https://github.com/MrTopia/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375 --depth=1
+git clone https://github.com/MrTopia/device_xiaomi_veux-kernel.git device/xiaomi/veux-kernel
+
+echo "Cloning Firmware tree..."
+# Firmware Tree
+git clone https://redkarmakar@bitbucket.org/mrtopia/vendor_xiaomi_veux-firmware.git vendor/xiaomi/veux-firmware
+
+echo "Cloning Vendor..."
+# Vendor
+git clone https://github.com/MrTopia/vendor_xiaomi_veux -b 14.0 vendor/xiaomi/veux
+
+echo "Cloning hardware related stuff..."
+# Hardware
+rm -rf hardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+
+echo "Cloning MIUI Camera..."
+# MIUI Camera
+git clone https://github.com/Positron-B/vendor_xiaomi_miuicamera-veux.git vendor/xiaomi/miuicamera-veux
+git clone https://github.com/MrTopia/vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
+cd vendor/xiaomi/veux/proprietary/system/priv-app/MiuiCamera/ && wget https://sourceforge.net/projects/tranquilacloud/files/MiuiCamera.apk && cd ../../../../../../..
+
+echo "Cloning Audio Configs..."
+# Audio Configs
+rm -rf hardware/qcom-caf/sm8350/audio/configs/holi && git clone https://github.com/MrTopia/hardware_qcom-caf_sm8350_audio_configs_holi.git -b parts hardware/qcom-caf/sm8350/audio/configs/holi
+
+echo "Completed, proceeding to lunch"
