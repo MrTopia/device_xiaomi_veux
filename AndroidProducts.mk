2124f33acc5983dfaddf7ad7ffbf1bf4c450ab12
MrTopia <redkarmakar@gmail.com>
2024-03-17 23:49:57 +0530

    veux: Welcome to AfterLifeOS
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_veux.mk
+    $(LOCAL_DIR)/afterlife_veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_veux-user \
-    aosp_veux-userdebug \
-    aosp_veux-eng
+    afterlife_veux-user \
+    afterlife_veux-userdebug \
+    afterlife_veux-eng
