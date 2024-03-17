e3b914b9004282bd52220922b45b27122db0f3d2
MrTopia <redkarmakar@gmail.com>
2024-03-17 23:49:57 +0530

    veux: Welcome to AlphaDroid
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_veux.mk
+    $(LOCAL_DIR)/lineage_veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_veux-user \
-    aosp_veux-userdebug \
-    aosp_veux-eng
+    lineage_veux-user \
+    lineage_veux-userdebug \
+    lineage_veux-eng
