d9f9640252cd275060e356c24a791b015dd62e20
Michael Bestas <mkbestas@lineageos.org>
2024-03-28 00:06:56 +0200

    veux: overlay: Enable battery cycle count
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- device.mk
+++ device.mk
@@ -290,6 +290,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    LineageSettingsOverlayVEUX \
+
+PRODUCT_PACKAGES += \
     CarrierConfigOverlayCOMMON \
     FrameworkOverlayCOMMON \
     TelephonyOverlayCOMMON
