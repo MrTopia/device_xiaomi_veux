0419cd611c61c06a5b3e9144e9983644b0c5d02a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-04-21 22:47:17 +0530

    veux: overlay: Enable VoNR for Jio 5G and Calls
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,6 +157,13 @@ TARGET_BOARD_PLATFORM := holi
 # Power
 TARGET_TAP_TO_WAKE_NODE := /proc/tp_gesture
 
+# Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Recovery
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.default
