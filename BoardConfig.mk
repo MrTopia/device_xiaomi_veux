2d75b589e2fdc004c12052f349a18eb65284fc12
Frost <159105703+Karan-Frost@users.noreply.github.com>
2024-05-01 07:05:47 +0530

    veux: props: Move to proper path
    - Minor Cleanup
    - Add system.prop
    - Copy out props from BoardConfig.mk
    
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,6 +161,13 @@ TARGET_BOARD_PLATFORM := holi
 # Power
 TARGET_TAP_TO_WAKE_NODE := /proc/tp_gesture
 
+# Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/properties/system_ext.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
+
 # Recovery
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.default
