b3a7b5f5a64f943b67fdb6af16303338ab47295f
Bruno Martins <bgcngm@gmail.com>
2024-06-16 15:14:57 +0100

    veux: Inherit from common Xiaomi FCM
    Change-Id: I8d4d54bba1b3dff27c11b0034852697b818ee609
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,7 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
