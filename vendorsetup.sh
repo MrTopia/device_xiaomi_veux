f460c51881341bffe588ab85f8b8b42c97dc84c9
MrTopia <redkarmakar@gmail.com>
2024-08-01 21:14:36 +0000

    veux: Add Support for ViPER4AndroidFX
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,7 +10,7 @@ git clone https://gitea.com/MrTopia/vendor_xiaomi_veux-firmware.git vendor/xiaom
 
 echo "Cloning Vendor..."
 # Vendor
-git lfs clone https://x-token-auth:ATCTT3xFfGN0Uahs70EPu15B2oO7aRdu5CI8QCkHtWkb60PXpfO4mg8kMfZUkkDpoUF3t-S-fp3Ivs5uveq1dHy_sH66lxFDtRdBFKj7S6_UaMnxs4E8wtKDzNdYuYY1enWJiAreTCETfRWWLpa0WAjEfsryjrHecVlxKaZTWlxZtUCAwjqX-UM=0BDEEE0F@bitbucket.org/mrtopia/vendor_xiaomi_veux.git vendor/xiaomi/veux
+git lfs clone https://x-token-auth:ATCTT3xFfGN0Uahs70EPu15B2oO7aRdu5CI8QCkHtWkb60PXpfO4mg8kMfZUkkDpoUF3t-S-fp3Ivs5uveq1dHy_sH66lxFDtRdBFKj7S6_UaMnxs4E8wtKDzNdYuYY1enWJiAreTCETfRWWLpa0WAjEfsryjrHecVlxKaZTWlxZtUCAwjqX-UM=0BDEEE0F@bitbucket.org/mrtopia/vendor_xiaomi_veux.git -b parts vendor/xiaomi/veux
 
 echo "Cloning hardware related stuff..."
 # Hardware
@@ -23,7 +23,7 @@ git clone https://github.com/MrTopia/vendor_xiaomi_miuicamera.git vendor/xiaomi/
 
 echo "Cloning Audio Configs..."
 # Audio Configs
-rm -rf hardware/qcom-caf/sm8350/audio/configs/holi && git clone https://github.com/MrTopia/hardware_qcom-caf_sm8350_audio_configs_holi.git hardware/qcom-caf/sm8350/audio/configs/holi
+rm -rf hardware/qcom-caf/sm8350/audio/configs/holi && git clone https://github.com/MrTopia/hardware_qcom-caf_sm8350_audio_configs_holi.git -b parts hardware/qcom-caf/sm8350/audio/configs/holi
 
 echo "Cloning Sign keys..."
 # Keys
@@ -32,5 +32,10 @@ cd vendor/everest/signing/keys/ && rm -rf BUILD && git clone https://github.com/
 echo "Cloning BCR..."
 # BCR
 git clone https://github.com/MrTopia/vendor_bcr vendor/bcr
+
+echo "Clonimg ViPER......."
+# ViPER
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
+
 
 echo "Completed, proceeding to lunch"
