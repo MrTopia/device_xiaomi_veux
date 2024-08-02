d49980fcbe120be8869af1c983f75118ef4f2d2e
MrTopia <redkarmakar@gmail.com>
2024-08-02 19:46:49 +0530

    veux: Bring Up AfterLifeOS (2/2)
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -6,7 +6,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
     AmbientSensePrebuilt \
-    Aperture \
     arcore \
     Chrome \
     Chrome-Stub \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,25 +2,17 @@ echo "Cloning required stuffs..."
 
 echo "Cloning Kernel..."
 # Kernel
-git clone https://github.com/MrTopia/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375 --depth=1
+git clone https://github.com/derefernce23/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375 --depth=1
 
-echo "Cloning Firmware tree..."
-# Firmware Tree
-git clone https://redkarmakar@bitbucket.org/mrtopia/vendor_xiaomi_veux-firmware.git vendor/xiaomi/veux-firmware
 
 echo "Cloning Vendor..."
 # Vendor
-git clone https://github.com/MrTopia/vendor_xiaomi_veux -b udc-wip vendor/xiaomi/veux
-
-echo "Cloning hardware related stuff..."
-# Hardware
-rm -rf hardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone https://github.com/MrTopia/android_vendor_xiaomi_veux -b fourteen vendor/xiaomi/veux
 
 echo "Cloning MIUI Camera..."
 # MIUI Camera
 git clone https://github.com/Positron-B/vendor_xiaomi_miuicamera-veux.git vendor/xiaomi/miuicamera-veux
-git clone https://github.com/MrTopia/vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
-cd vendor/xiaomi/veux/proprietary/system/priv-app/MiuiCamera/ && wget https://sourceforge.net/projects/tranquilacloud/files/MiuiCamera.apk && cd ../../../../../../..
+git clone https://github.com/Positron-B/vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
 
 echo "Cloning Audio Configs..."
 # Audio Configs
