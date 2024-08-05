b738401de5d9eeb2fcb4c7656dca9f28f3342500
MrTopia <redkarmakar@gmail.com>
2024-08-06 02:21:38 +0530

    veux: Bring Up AfterlifeOS (1/2)
    Signed-off-by: MrTopia <redkarmakar@gmail.com>

---
+++ afterlife_veux.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2023-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0 
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
+
+PRODUCT_BRAND := Redmi
+PRODUCT_DEVICE := veux
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 2201116SG
+PRODUCT_NAME := afterlife_veux
+PRODUCT_SYSTEM_NAME := veux_global
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="veux_global-user 13 TKQ1.221114.001 V816.0.3.0.TKCMIXM release-keys" \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := Redmi/veux_global/veux:13/TKQ1.221114.001/V816.0.3.0.TKCMIXM:user/release-keys
+
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Maintainer
+AFTERLIFE_MAINTAINER := MrTopiA
