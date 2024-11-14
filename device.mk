de296c6b820e906fb76daeea3e65c1727ee08d9d
Frost <159105703+Karan-Frost@users.noreply.github.com>
2024-11-14 16:09:13 +0530

    Fixup! veux: Do not manually build dependencies
    This fixes commit d66f2e0fb3edb43fe95583ea354de4d920f3aee1.
    
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -120,10 +120,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
-PRODUCT_PACKAGES += \
-    com.fingerprints.extension@1.0.vendor \
-    libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
-
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
